cf4c330b4b476e71df6a81ed8b3c5045d1f1404f
ttjm792866257 <6261093@qq.com>
2017-09-29 10:30:25 +0800

    create Classify

---
+++ src/components/Quality_life.vue
@@ -0,0 +1,136 @@
+<template>
+    <div class="quality_life_cont content_box">
+			<div class="kitchen_list">
+				<h2>
+					<a href="###">
+						厨具
+						<span>更多 &gt;</span>
+					</a>
+				</h2>
+				<div class="class_cont clear" >
+					<a href="###">
+						<dl>
+							<dt><img src="https://cdn.bestkeep.cn/goods/img/goods_1468488891880.jpg" class="full"></dt>
+							<dd>烹饪锅具</dd>
+						</dl>
+					</a>
+					<a href="###">
+						<dl>
+							<dt><img src="https://cdn.bestkeep.cn/goods/img/goods_1469435727993.jpg" class="full"></dt>
+							<dd>刀剪菜板</dd>
+						</dl>
+					</a>
+					<a href="###">
+						<dl>
+							<dt><img src="https://cdn.bestkeep.cn/goods/img/goods_1472527408473.jpg" class="full"></dt>
+							<dd>厨房配件</dd>
+						</dl>
+					</a>
+					<a href="###">
+						<dl>
+							<dt><img src="https://cdn.bestkeep.cn/goods/img/goods_1470722657742.jpg" class="full"></dt>
+							<dd>水具酒具</dd>
+						</dl>
+					</a>
+					<a href="###">
+						<dl>
+							<dt><img src="https://cdn.bestkeep.cn/goods/img/goods_1468404364830.jpg" class="full"></dt>
+							<dd>餐具厨具</dd>
+						</dl>
+					</a>
+					<a href="###">
+						<dl>
+							<dt><img src="https://cdn.bestkeep.cn/goods/img/goods_1468488648432.jpg" class="full"></dt>
+							<dd>茶具/咖啡具</dd>
+						</dl>
+					</a>
+					<div class="blank"></div>
+				</div>
+			</div>
+			<div class="clear_goods_list">
+				<h2>
+					<a href="###">
+						清洁用品
+						<span>更多 &gt;</span>
+					</a>
+				</h2>
+				<div class="class_cont clear" >
+					<a href="###">
+						<dl>
+							<dt><img src="https://cdn.bestkeep.cn/goods/img/goods_1468404404094.jpg" class="full"></dt>
+							<dd>衣物清洁</dd>
+						</dl>
+					</a>
+					<a href="###">
+						<dl>
+							<dt><img src="https://cdn.bestkeep.cn/goods/img/goods_1468404418741.jpg" class="full"></dt>
+							<dd>驱蚊/驱虫用品</dd>
+						</dl>
+					</a>
+					<a href="###">
+						<dl>
+							<dt><img src="https://cdn.bestkeep.cn/goods/img/goods_1468404435475.jpg" class="full"></dt>
+							<dd>家庭清洁</dd>
+						</dl>
+					</a>
+					<a href="###">
+						<dl>
+							<dt><img src="https://cdn.bestkeep.cn/goods/img/goods_1468488947272.jpg" class="full"></dt>
+							<dd>清洁工具</dd>
+						</dl>
+					</a>
+					<div class="blank"></div>
+				</div>
+			</div>
+			<div class="life_day_list">
+				<h2>
+					<a href="###">
+						生活日用
+						<span>更多 &gt;</span>
+					</a>
+				</h2>
+				<div class="class_cont clear" >
+					<a href="###">
+						<dl>
+							<dt><img src="https://cdn.bestkeep.cn/goods/img/goods_1468489216971.jpg" class="full"></dt>
+							<dd>雨伞雨具</dd>
+						</dl>
+					</a>
+					<a href="###">
+						<dl>
+							<dt><img src="https://cdn.bestkeep.cn/goods/img/goods_1468488960465.jpg" class="full"></dt>
+							<dd>收纳/洗晒</dd>
+						</dl>
+					</a>
+					<a href="###">
+						<dl>
+							<dt><img src="https://cdn.bestkeep.cn/goods/img/goods_1468404533175.jpg" class="full"></dt>
+							<dd>生活用品</dd>
+						</dl>
+					</a>
+					<a href="###">
+						<dl>
+							<dt><img src="https://cdn.bestkeep.cn/goods/img/goods_1473216978067.jpg" class="full"></dt>
+							<dd>汽车用品</dd>
+						</dl>
+					</a>
+					<div class="blank"></div>
+				</div>
+			</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Quality_life",
+  data () {
+    return {
+
+    };
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  @import '/../static/css/classify_cont.css';
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import Hello from '@/components/Hello'
 // 导入购物车模块
 import Cart from '@/pages/Cart'
+// 导入分类模块
+import Classify from '@/pages/Classify'
 
 
 Vue.use(Router)
@@ -10,7 +12,7 @@ Vue.use(Router)
 export default new Router({
     routes: [{
         path: '/',
-        name: 'Cart',
-        component: Cart,
+        name: 'Classify',
+        component: Classify,
     }]
 })
