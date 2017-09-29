dfee0b7a52baa6c0175015b1c723ba2b51bbf589
lanlan2346 <1598017061@qq.com>
2017-09-29 15:06:40 +0800

    modified : data

--- src/router/index.js
+++ src/router/index.js
@@ -2,32 +2,31 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 
-
-// import Hello from '@/components/Hello'
-// import Mine from '../pages/Mine'
-// import Home from '../pages/Home'
-// import Referral from '../components/Referral'
+import Hello from '@/components/Hello'
+import Mine from '../pages/Mine'
+import Home from '../pages/Home'
+import Referral from '../components/Referral'
 
 // import Product from '@/components/pages/Product'
-import Stype from '@/components/Stype'
+// import Stype from '@/components/Stype'
 
 Vue.use(Router)
 
 
 export default new Router({
   routes: [
-    // {path: '/', component: Home},
-    // {path: '/home', component: Home, children: [
-    //   {path: '/', component: Referral},
-    //   {path: 'referral', component: Referral}
-    // ]},
-    // {path: '/classify', component: Mine},
-    // {path: '/cart', component: Mine},
-    // {path: '/mine', component: Mine},
+    {path: '/', component: Home},
+    {path: '/home', component: Home, children: [
+      {path: '/', component: Referral},
+      {path: 'referral', component: Referral}
+    ]},
+    {path: '/classify', component: Mine},
+    {path: '/cart', component: Mine},
+    {path: '/mine', component: Mine},
 
-    {path: '/',
-    name:'Stype',
-    component:Stype}
+    // {path: '/',
+    // name:'Stype',
+    // component:Stype}
 
   ],
   mode: 'history'
